ffe65459c5aa41e66bfad9c898acfef2a784742d
JULLYVUI <vuitrannb@gmail.com>
2022-08-16 14:06:51 +0700

    cd angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Router, RouterEvent } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  activePath = '';
+
+  pages = [
+    {
+      name: 'Tab 1',
+      path: '/tabs/tab1'
+    },
+    {
+      name: 'Tab 2',
+      path: '/tabs/tab2'
+    },
+    {
+      name: 'Tab 3',
+      path: '/tabs/tab3'
+    }
+  ];
+  constructor(private translate: TranslateService,private router: Router) {
+    this.router.events.subscribe((event: RouterEvent) => {
+      this.activePath = event.url;
+    });
+  }
+
+  public get currentLanguage(): string {
+    return this.translate.currentLang;
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'jp'
+    })
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
